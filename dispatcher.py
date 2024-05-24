88a3ef02bd15e7b4ec7388530ebdd33b3018d32a
gro225 <antonvorobyev2004@yandex.ru>
2024-05-24 16:29:11 +0300

    bd connection

--- dispatcher.py
+++ dispatcher.py
@@ -1,14 +1,19 @@
 import logging
 from aiogram import Bot, Dispatcher
 import config
+from database import create_pool
 
 # Конфигурация логирования 
 logging.basicConfig(level=logging.INFO)
 
 # Предзапрос 
 if not config.BOT_TOKEN:
-    exit("No token")
+    exit("No token provided")
 
 # init
 bot = Bot(token=config.BOT_TOKEN, parse_mode="HTML")
 dp = Dispatcher()
+
+# Соединение с бд
+async def on_startup():
+    dp['db'] = await create_pool()
